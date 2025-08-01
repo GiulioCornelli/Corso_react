044a09b018380e2c4ab595c912fee374c1ab837b
GiulioCornelli <cornelli.giulio05@gmail.com>
2025-08-01 09:25:50 +0200

    lezione 4

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import './Navbar.css';
+
+export function Navbar (){
+    return(
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/services">Services</a></li>
+        </ul>
+    );
+}
+
